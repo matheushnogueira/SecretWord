739d704bb998b9e24275812f561a50bf5368805d
Mafeushns <matheusnog001@gmail.com>
2022-10-07 10:06:15 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+//Components
+import StartScreen from './components/StartScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <StartScreen/>
+    </div>
+  );
+}
+
+export default App;
